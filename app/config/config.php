17cf696b7dd0345715a455c5d43ef2a3c3c77bdf
Fabiano Oliveira <fabiano.one@gmail.com>
2022-10-18 22:50:22 -0300

    First commit

---
+++ app/config/config.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Config
+ * php version 7.4^
+ *
+ * @category Define_Constants
+ * @package  Require
+ * @author   Fabiano Oliveira <fabiano.one@gmail.com>
+ * @license  GNU http://opensource.org/licenses/gpl-license.php
+ * @version  GIT: @1.0.0@
+ * @link     https://github.com/fabianoone
+ */
+define('DB_HOST', 'localhost');
+define('DB_USER', '_YOUR_DB_USERNAME_');
+define('DB_PASS', '_YOUR_DB_PASSWORD_');
+define('DB_NAME', '_YOUR_DBNAME_');
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL Root
+define('URLROOT', '_YOUR_APP_URL_');
+
+// App Name
+define('APPNAME', '_YOUR_APP_NAME_');
